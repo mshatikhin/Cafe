d7ba75d86e39fb574996727bb01eb831f089adc3
mike <mike@iMac-mike.local>
2016-07-28 22:29:06 +0500

    Test

--- index.ios.js
+++ index.ios.js
@@ -17,10 +17,10 @@ class Cafe extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+         Cafe!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          Test
         </Text>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
